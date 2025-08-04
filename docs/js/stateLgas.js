00256f76d7875c4f165c0b43bd8f517e0bcd4ec1
Veegilinfrastructure <infrastructure@veegil.com>
2025-08-04 02:38:29 +0100

    Update stateLgas.js

--- docs/js/stateLgas.js
+++ docs/js/stateLgas.js
@@ -1,17 +1,17 @@
 const stateLgas = {
-  "Abia": [
+  "abia": [
     "Aba North", "Aba South", "Arochukwu", "Bende", "Ikwuano",
     "Isiala Ngwa North", "Isiala Ngwa South", "Isuikwuato", "Obi Ngwa", "Ohafia",
     "Osisioma", "Ugwunagbo", "Ukwa East", "Ukwa West", "Umuahia North",
     "Umuahia South", "Umu Nneochi"
   ],
-  "Adamawa": [
+  "adamawa": [
     "Demsa", "Fufore", "Ganye", "Girei", "Gombi", "Guyuk", "Hong",
     "Jada", "Lamurde", "Madagali", "Maiha", "Mayo-Belwa", "Michika",
     "Mubi North", "Mubi South", "Numan", "Shelleng", "Song", "Toungo",
     "Yola North", "Yola South"
   ],
-  "Akwa Ibom": [
+  "akwa Ibom": [
     "Abak", "Eastern Obolo", "Eket", "Esit Eket", "Essien Udim", "Etim Ekpo",
     "Etinan", "Ibeno", "Ibesikpo Asutan", "Ibiono Ibom", "Ika", "Ikono",
     "Ikot Abasi", "Ikot Ekpene", "Ini", "Itu", "Mbo", "Mkpat-Enin",
@@ -19,41 +19,41 @@ const stateLgas = {
     "Onna", "Oron", "Oruk Anam", "Udung-Uko", "Ukanafun", "Uruan",
     "Urue-Offong/Oruko", "Uyo"
   ],
-  "Anambra": [
+  "anambra": [
     "Aguata", "Anambra East", "Anambra West", "Anaocha", "Awka North",
     "Awka South", "Ayamelum", "Dunukofia", "Ekwusigo", "Idemili North",
     "Idemili South", "Ihiala", "Njikoka", "Nnewi North", "Nnewi South",
     "Ogbaru", "Onitsha North", "Onitsha South", "Orumba North", "Orumba South",
     "Oyi"
   ],
-  "Bauchi": [
+  "bauchi": [
     "Alkaleri", "Bauchi", "Bogoro", "Damban", "Darazo", "Dass", "Gamawa",
     "Ganjuwa", "Giade", "Itas/Gadau", "Jama'are", "Katagum", "Kirfi",
     "Misau", "Ningi", "Shira", "Tafawa Balewa", "Toro", "Warji", "Zaki"
   ],
-  "Bayelsa": [
+  "bayelsa": [
     "Brass", "Ekeremor", "Kolokuma/Opokuma", "Nembe", "Ogbia",
     "Sagbama", "Southern Ijaw", "Yenagoa"
   ],
-  "Benue": [
+  "benue": [
     "Ado", "Agatu", "Apa", "Buruku", "Gboko", "Guma", "Gwer East",
     "Gwer West", "Katsina-Ala", "Konshisha", "Kwande", "Logo",
     "Makurdi", "Obi", "Ogbadibo", "Ohimini", "Oju", "Okpokwu",
     "Otukpo", "Tarka", "Ukum", "Ushongo", "Vandeikya"
   ],
-  "Borno": [
+  "borno": [
     "Abadam", "Askira/Uba", "Bama", "Bayo", "Biu", "Chibok", "Damboa",
     "Dikwa", "Gubio", "Guzamala", "Gwoza", "Hawul", "Jere", "Kaga",
     "Kala/Balge", "Konduga", "Kukawa", "Kwaya Kusar", "Mafa",
     "Magumeri", "Maiduguri", "Marte", "Mobbar", "Monguno",
     "Ngala", "Nganzai", "Shani"
   ],
-  "Cross River": [
+  "cross River": [
     "Abi", "Akamkpa", "Akpabuyo", "Bakassi", "Bekwarra", "Biase",
     "Boki", "Calabar Municipal", "Calabar South", "Etung", "Ikom",
     "Obanliku", "Obubra", "Obudu", "Odukpani", "Ogoja", "Yakurr", "Yala"
   ],
-  "Delta": [
+  "delta": [
     "Aniocha North", "Aniocha South", "Bomadi", "Burutu", "Ethiope East",
     "Ethiope West", "Ika North East", "Ika South", "Isoko North",
     "Isoko South", "Ndokwa East", "Ndokwa West", "Okpe", "Oshimili North",
@@ -202,3 +202,4 @@ const stateLgas = {
     "Abaji", "Bwari", "Gwagwalada", "Kuje", "Kwali", "Municipal Area Council"
   ]
 };
+
